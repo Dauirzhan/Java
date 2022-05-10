ee61b560eae35d3f50816dfc24a67b9d3b081c76
Dauirzhan <raevdauir00@gmail.com>
2022-05-11 00:18:42 +0600

    Add files via upload

--- src/Country.java
+++ src/Country.java
@@ -1,17 +1,103 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Country {
-    private static List<City> getCity() {
-        return List.of(
-                new City(1, "London", 1, 11_000_000),
-                new City(1, "Bayern", 6, 8_000_000),
-                new City(1, "Tokyo", 10, 30_000_000),
-                new City(1, "Madrid", 8, 1_600_000),
-                new City(1, "Milan", 7, 4_000_000),
-                new City(1, "Almaty", 5, 2_100_000),
-                new City(1, "Melburn", 3, 18_000_000)
+    String city, capital, country;
 
-        );
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public Country(String country) {
+        this.country = country;
+    }
+
+    private List<City> citiesList = new ArrayList<>();
+
+    public Country(String city, String capital, String country, List<City> citiesList) {
+        this.city = city;
+        this.capital = capital;
+        this.country = country;
+        this.citiesList = citiesList;
+    }
+
+    Country(String city, String capital, String country) {
+        this.city = city;
+        this.capital = capital;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public List<City> getCitiesList() {
+        return citiesList;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public void setCitiesList(List<City> citiesList) {
+        this.citiesList = citiesList;
+    }
+
+}
+
+class City {
+    private int id;
+    private int population;
+    private String name;
+    private int CountryCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCountryCode() {
+        return CountryCode;
+    }
+
+    public void setCountryCode(int countryCode) {
+        CountryCode = countryCode;
+    }
+
+    public City(int id, int population, String name, int countryCode) {
+        this.id = id;
+        this.population = population;
+        this.name = name;
+        this.CountryCode = countryCode;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,51 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
+        public static void main(String[] args) {
+                List<Country> countries = getCountry();
+                List<City> cities = getCity();
+
+                List<City> sorted = cities.stream()
+                        .sorted(Comparator.comparing(City::getPopulation))
+                        .collect(Collectors.toList());
+                sorted.forEach(System.out::println);
+
+                cities.stream()
+                        .max(Comparator.comparing(City::getPopulation))
+                        .ifPresent((System.out::println));
+
+                List<Country> sorted1 = countries.stream()
+                        .sorted(Comparator.comparing(Country::getCity))
+                        .collect(Collectors.toList());
+                sorted.forEach(System.out::println);
+
+                countries.stream()
+                        .max(Comparator.comparing(Country::getCity))
+                        .ifPresent(System.out::println);
+        }
+        private static List<Country> getCountry(){
+                return List.of(
+                        new Country("Almaty", "Nur-Sultan", "Kazakhstan"),
+                        new Country("Manchester", "London", "GreatBritain"),
+                        new Country("Marsel", "Paris", "France"),
+                        new Country("Milan>", "Rome", "Italy"),
+                        new Country("Barcelona", "Madrid", "Spain")
+                );
+        }
+        private static List<City> getCity(){
+                return List.of(
+                        new City(1,  2200000, "ALmaty", 5),
+                        new City(2,  4800000, "Manchester", 1),
+                        new City(3,  3500000,"Marsel", 5),
+                        new City(4,  6100000,"Milan", 5),
+                        new City(5,  5900000, "Barcelona", 5)
+                );
+        }
+
     /*static void Mymethod(String fname, int age){
         System.out.println(fname + " is " + age);
     }
@@ -38,14 +82,4 @@ public class Main {
 
      */
 
-    public static void main(String[] args) {
-
-
-    }
-    private static List<Country> getCountry(){
-        return List.of(
-                new Country()
-        );
-
-    }
 }
